30a59c3dd74afc7e09e0cb7833006905fb6689db
GatoReno <dmn@DMNs-MacBook-Pro.local>
2019-06-20 11:10:21 -0500

    agregando rutas de prueba para datos en app mobil

--- src/routes/index.js
+++ src/routes/index.js
@@ -57,7 +57,7 @@ router.get('/get-user/:id',(req,res)=>{
     const {id} = req.params;
     
     
-    const query = pool.query('SELECT * FROM USERS_ where id = ',[id]);
+    const query = pool.query('SELECT * FROM USERS_ where id = ?',[id]);
     query.then((data)=>{
         
         const userinfo = data[0];
